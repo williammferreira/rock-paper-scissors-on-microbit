3cd0458b0e25bd6a08a4de96fa8223db78f3c83a
William Ferreira <82338455+williammferreira@users.noreply.github.com>
2022-01-25 09:22:55 -0500

    Finished

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-let Bit = 0
-input.onGesture(Gesture.Shake, function () {
+Bit = 0
+
+def on_gesture_shake():
+    global Bit
     Bit = randint(0, 2)
-    if (Bit == 0) {
-        basic.showIcon(IconNames.SmallSquare)
-    } else if (0 == 1) {
-        basic.showIcon(IconNames.Square)
-    } else {
-        basic.showIcon(IconNames.Scissors)
-    }
-})
-basic.forever(function () {
-	
-})
+    if Bit == 0:
+        basic.show_icon(IconNames.SMALL_SQUARE)
+    elif Bit == 1:
+        basic.show_icon(IconNames.SQUARE)
+    else:
+        basic.show_icon(IconNames.SCISSORS)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
